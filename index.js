11a352f0d79f83cfb971e3643e556cf9bd20e618
S00201014-ATU <S00201014@atu.ie>
2023-09-21 15:29:40 +0100

    Try a second link with name 'test'

--- index.js
+++ index.js
@@ -7,3 +7,5 @@ const port = 3000
 app.get('/', (req, res) => res.send("Hello World From Matthew"))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
+
+app.get('/test', (req, res) => res.send('Hello World this is a test'));
